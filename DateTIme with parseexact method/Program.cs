0b8c2ecfdc2e8901f08763c408b7783967652f4e
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-06 16:50:20 +0400

    Datetime created with parseexact method

---
+++ DateTIme with parseexact method/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Globalization;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        DateTime  datetime = DateTime.ParseExact("06_07_2023", "dd_MM_yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine(datetime);
+		try
+		{
+
+		}
+		catch (Exception ex)
+		{
+            Console.WriteLine(ex.Message);
+        }
+        Console.Read();
+    }
+}
+
